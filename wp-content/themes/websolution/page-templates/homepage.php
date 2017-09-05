dedb29a26ede458e8b5b43b5d01b6f96ca129887
=krunchness <=enjay.navarro@gmail.com>
2017-09-05 17:33:12 +0800

    changes

--- wp-content/themes/websolution/page-templates/homepage.php
+++ wp-content/themes/websolution/page-templates/homepage.php
@@ -165,80 +165,82 @@ get_header(); ?>
 					<h2>Check Our Recent Projects</h2>
 				</div>
 				<div class="col-md-12">
+					<div class="da-container">
 					<ul id="da-thumbs" class="da-thumbs">
 						<li>
 							<a href="http://dribbble.com/shots/505046-Menu">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/1.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>Menu by Simon Jensen</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/504336-TN-Aquarium">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/2.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>TN Aquarium by Charlie Gann</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/504197-Mr-Crabs">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/3.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>Mr. Crabs by John Generalov</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/503731-Gallery-of-Mo-2-Mo-logo">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/4.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>Gallery of Mo 2.Mo logo by Adam Campion</span></div>
 							</a>
 						</li>
 						<li>	
 							<a href="http://dribbble.com/shots/503058-Ice-Cream-nom-nom">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/5.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>Ice Cream - nom nom by Eight Hour Day</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/502927-My-Muse">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/6.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>My Muse by Zachary Horst</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/7.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>Natalie & Justin Cleaning by Justin Younger</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/502523-App-Preview">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/8.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>App Preview by Ryan Deshler</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/501695-Cornwall-Map">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/9.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>Cornwall Map by Katharina Maria Zimmermann</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/500861-final-AD-logo">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/10.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>final AD logo by Annette Diana</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/500369-Land-Those-Planes">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/11.jpg" />
+								<img src="<?php echo get_template_directory_uri(); ?>/images/comingsoon.png" />
 								<div><span>Land Those Planes by Lee Ann Marcel</span></div>
 							</a>
 						</li>
 						<li>
 							<a href="http://dribbble.com/shots/497795-Seahorse">
-								<img src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/12.jpg" />
+								<img class="grayscale-filter" src="<?php echo get_template_directory_uri(); ?>/inc/directionhover/images/12.jpg" />
 								<div><span>Seahorse by Trevor Basset</span></div>
 							</a>
 						</li>
 					</ul>
+					</div>
 				</div>
 			</div>
 		</div>
